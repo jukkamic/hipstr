56c808b260908e07174b74e320f6480bf46c2a52
Jukka Michelsson <jukkamic@gmail.com>
2015-12-31 00:39:09 -0600

    testing new subname for heroku

--- src/hipstr/models/connection.clj
+++ src/hipstr/models/connection.clj
@@ -4,6 +4,6 @@
 (def db-spec {:classname   "org.postgresql.Driver"
               :subprotocol "postgresql"
               ;;:subname     "postgres"
-	      :subname "ec2-107-22-170-249.compute-1.amazonaws.com:5432/d3qe27609gdkmm"
+	      :subname "//ec2-107-22-170-249.compute-1.amazonaws.com:5432/d3qe27609gdkmm"
               :user        (or (System/getenv "JDBC_DATABASE_USERNAME") "hipstr")
               :password    (or (System/getenv "JDBC_DATABASE_PASSWORD") "hipstr")})
