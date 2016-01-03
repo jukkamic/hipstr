0edefc9ffa75f7c56a57512f96c7ff67feea64d3
Jukka Michelsson <jukkamic@gmail.com>
2016-01-02 20:10:36 -0600

    testing

--- src/hipstr/models/connection.clj
+++ src/hipstr/models/connection.clj
@@ -4,7 +4,9 @@
 (def db-spec {:classname   "org.postgresql.Driver"
               :subprotocol "postgresql"
               ;;:subname     "postgres"
-	      ;:subname "//ec2-107-22-170-249.compute-1.amazonaws.com:5432/d3qe27609gdkmm"
-	      	  :subname (System/getenv "JDBC_DATABASE_URL")
-              :user        (or (System/getenv "JDBC_DATABASE_USERNAME") "hipstr")
-              :password    (or (System/getenv "JDBC_DATABASE_PASSWORD") "hipstr")})
+	      	  :subname "//ec2-107-22-170-249.compute-1.amazonaws.com:5432/d3qe27609gdkmm"
+	      	  ;:subname (System/getenv "JDBC_DATABASE_URL")
+              ;:user        (or (System/getenv "JDBC_DATABASE_USERNAME") "hipstr")
+              ;:password    (or (System/getenv "JDBC_DATABASE_PASSWORD") "hipstr")})
+              :user        (env :jdbc-database-username)
+			  :password    (env :jdbc-database-password)})
